68b181e0fdfde0eef4295c41c4030cf2891e4473
Pranu Sarna <pranusarna@gmail.com>
2015-06-25 19:11:04 +0530

    Ch 04 completed. started with ch 05

--- ch.04-Data Structures Objects and Arrays/Problem 3.js
+++ ch.04-Data Structures Objects and Arrays/Problem 3.js
@@ -1,13 +1,30 @@
-var arrayToList = function(array){
-	var list = null
-	for(var i=0; i<array.length; i++){
-		
-		var store = array.pop();
-		list = {
-			value: store,
-			rest: list
+function arrayToList(array) {
+  var list = null;
+  for (var i = array.length - 1; i >= 0; i--)
+    list = {value: array[i], rest: list};
+  return list;
+}
+
+var prepend = function(value, list){
+	return {
+		value: value,
+		rest: list
 		}
+}
 
-
+var listToArray = function(list){
+	var array=[];
+	for (var node=list; node; node = node.rest){
+		array.push(node.value)
 	}
+return array
+}
+
+function nth(list, n) {
+  if (!list)
+    return undefined;
+  else if (n == 0)
+    return list.value;
+  else
+    return nth(list.rest, n - 1);
 }
